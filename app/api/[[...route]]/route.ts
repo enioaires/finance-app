9fcea6bcdb08aaa3dc6c7e11630beb327c9f1ba4
Enio Aires <enio.aires@hotmail.com>
2024-05-19 12:40:23 -0300

    drizzle rpc

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,17 +1,17 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-
-import authors from "./authors";
-import books from "./books";
 import { clerkMiddleware } from "@hono/clerk-auth";
+
+import accounts from "./accounts";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 app.use("*", clerkMiddleware());
 
-app.route("/authors", authors);
-app.route("/books", books);
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
