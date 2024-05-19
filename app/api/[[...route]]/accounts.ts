b4432f477f31f1c2ea4963bdca374223034a5fef
Enio Aires <enio.aires@hotmail.com>
2024-05-19 13:09:26 -0300

    react query

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,7 +1,30 @@
 import { Hono } from "hono";
+import { db } from "@/db/drizzle";
+import { accounts } from "@/db/schema";
+import { getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
 
-const app = new Hono().get("/", (ctx) => {
-  return ctx.json({ accounts: [] });
-});
+const app = new Hono().get(
+  "/",
+  async (ctx) => {
+    const auth = getAuth(ctx);
+
+    if (!auth?.userId) {
+      throw new HTTPException(401, {
+        res: ctx.json({ error: "Unauthorized" }),
+      });
+    }
+  },
+  async (ctx) => {
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts);
+
+    return ctx.json({ data });
+  }
+);
 
 export default app;
